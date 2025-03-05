85217b580ab5fc9d2e63a6f29163e64b400f7bdd
Rohan Mishra <romishra12@gmail.com>
2025-03-04 22:03:18 -0600

    First Git Commit! Cool!

--- CODE/export MVP Winners.R
+++ CODE/export MVP Winners.R
@@ -5,3 +5,5 @@ library(nbastatR)
 
 x <- bref_awards_votes(seasons=2000:2024)
 write.csv(dataMVPVotes, "./DATA/MVPWinners_2000-2024.csv")
+
+write.csv(dataMIPVotes, "./DATA/MIPWinners_2000-2024.csv")
